c97cd1ddd865c7fb37984b7afc4b1d1a9e4d6ced
Jose galindo <jmgalindob@eafit.edu.co>
2021-09-21 10:35:16 -0500

    Se clono el repos y modif scene CR

--- my-mate/Assets/Scripts/MoveSteps.cs
+++ my-mate/Assets/Scripts/MoveSteps.cs
@@ -6,6 +6,7 @@ public class MoveSteps : MonoBehaviour
 {
     void OnMouseDrag()
     {
+        Debug.Log("Esta arrastrando algo");
         float distance_to_screen = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
         Vector3 pos_move = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance_to_screen ));
         transform.position = new Vector3( pos_move.x, pos_move.y, pos_move.z );
