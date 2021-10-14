fc0c64f8799363bc2d58a2e0ebb4cec0fc1431e8
sospinai7 <sospinai@eafit.edu.co>
2021-10-14 11:52:58 -0500

    DISK BOUNCING PHYSICS

--- my-mate/Assets/Scripts/Disk.cs
+++ my-mate/Assets/Scripts/Disk.cs
@@ -4,97 +4,16 @@ using UnityEngine;
 
 public class Disk : MonoBehaviour
 {
-    /*
-    public int movementSpeed;
-    private void Update() {
-        if(Input.GetKey(KeyCode.A)){
-            Debug.Log("izquierda");
-            GetComponent<Rigidbody>().AddForce(new Vector3(movementSpeed, 0, 0));
-        }
-        if(Input.GetKey(KeyCode.D)){
-            Debug.Log("derecha");
-            GetComponent<Rigidbody>().AddForce(new Vector3(-movementSpeed, 0, 0));
-        }
-        if(Input.GetKey(KeyCode.W)){
-            Debug.Log("arriba");
-            GetComponent<Rigidbody>().AddForce(new Vector3(0, 0, movementSpeed));
-        }
-        if(Input.GetKey(KeyCode.S)){
-            Debug.Log("abajo");
-            GetComponent<Rigidbody>().AddForce(new Vector3(0, 0, -movementSpeed));
-        }
-    }
-    */
-    
     public GameObject Player;
     public GameObject disk;
-    public float movementSpeed;
+    public float magnitudePlayer;
+
+    public float magnitudeBorder;
     private bool moveDisk = false;
 
     private float gameObjectPositionX;
     private float gameObjectPositionY;
     private float gameObjectPositionZ;
-
-    void Update()
-    {
-        /*
-        if(Input.GetKey(KeyCode.A)){
-            Debug.Log("izquierda");
-            disk.GetComponent<Rigidbody>().AddForce(new Vector3(movementSpeed, 0, 0));
-        }
-        if(Input.GetKey(KeyCode.D)){
-            Debug.Log("derecha");
-            disk.GetComponent<Rigidbody>().AddForce(new Vector3(-movementSpeed, 0, 0));
-        }
-        if(Input.GetKey(KeyCode.W)){
-            Debug.Log("arriba");
-            disk.GetComponent<Rigidbody>().AddForce(new Vector3(0, 0, movementSpeed));
-        }
-        if(Input.GetKey(KeyCode.S)){
-            Debug.Log("abajo");
-            disk.GetComponent<Rigidbody>().AddForce(new Vector3(0, 0, -movementSpeed));
-        }
-*/
-        gameObjectPositionX = disk.transform.position.x;
-        gameObjectPositionY = disk.transform.position.y;
-        gameObjectPositionZ = disk.transform.position.z;
-/*
-        if (gameObjectPositionX > 1.9f)
-        {
-            gameObject.transform.position = new Vector3(1.9f, gameObjectPositionY, gameObjectPositionZ);
-        }
-        else if (gameObjectPositionX < -1.9f)
-        {
-            gameObject.transform.position = new Vector3(-1.9f, gameObjectPositionY, gameObjectPositionZ);
-        }
-
-        if (gameObjectPositionZ > 0)
-        {
-            gameObject.transform.position = new Vector3(gameObjectPositionX, gameObjectPositionY, 0);
-        }
-        else if (gameObjectPositionZ < -3.4f)
-        {
-            gameObject.transform.position = new Vector3(gameObjectPositionX, gameObjectPositionY, -3.4f);
-        }
-
-        if (gameObjectPositionZ > 0f && gameObjectPositionX > 1.9f)
-        {
-            gameObject.transform.position = new Vector3(1.9f, gameObjectPositionY, 0f);
-        }
-        else if (gameObjectPositionZ > 0f && gameObjectPositionX < -1.9f)
-        {
-            gameObject.transform.position = new Vector3(-1.9f, gameObjectPositionY, 0f);
-        }
-        else if (gameObjectPositionZ < -3.4f && gameObjectPositionX > 1.9f)
-        {
-            gameObject.transform.position = new Vector3(1.9f, gameObjectPositionY, -3.4f);
-        }
-        else if (gameObjectPositionZ < -3.4f && gameObjectPositionX < -1.9f)
-        {
-            gameObject.transform.position = new Vector3(-1.9f, gameObjectPositionY, -3.4f);
-        }
-        */
-    }
 
     void OnMouseDrag()
     {
@@ -112,26 +31,20 @@ public class Disk : MonoBehaviour
         // If the object we hit is the enemy
         if (other.gameObject.tag == "player")
         {  
-            var magnitude = 50;
+            //var magnitude = 50;
             var force = transform.position - other.transform.position;
             force.Normalize ();
-            disk.GetComponent<Rigidbody> ().AddForce (force * magnitude);
+            disk.GetComponent<Rigidbody> ().AddForce (force * magnitudePlayer);
             Debug.Log("Me toco el azul");
         }
-    }
 
-/*
-    private void OnTriggerEnter(Collider other)
-    {
-        // If the object we hit is the enemy
-        if (other.gameObject.tag == "player")
+        if (other.gameObject.tag == "border")
         {  
-            var magnitude = 50;
+            //var magnitude = 50;
             var force = transform.position - other.transform.position;
             force.Normalize ();
-            disk.GetComponent<Rigidbody> ().AddForce (force * magnitude);
-            Debug.Log("Me toco el azul");
+            disk.GetComponent<Rigidbody> ().AddForce (-force * magnitudeBorder);
+            Debug.Log("Me toco el borde");
         }
     }
-    */
 }
